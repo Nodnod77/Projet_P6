75a15a80e6f369640c1fd996b0d7968f7a5846fd
donia <donia.sakji@gmail.com>
2024-03-14 14:31:27 +0100

    StackNavigator

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {Text} from 'react-native';
+import {StackParamList} from "../navigation/StackNavigator";
+import {StackNavigationProp} from "@react-navigation/stack";
+import { Button } from 'react-native';
+import {NativeStackScreenProps} from "@react-navigation/native-stack";
+
+
+type HomeProps =  NativeStackScreenProps<StackParamList, 'HomeScreen'>;
+
+const HomeScreen = ({navigation}:HomeProps) => {
+    return (
+        <SafeAreaView>
+            <Text style = {{fontSize: 30}}>Hello</Text>
+            <Button title="click me ! ;))"
+                    onPress={()=> navigation.navigate("ActivityScreen")}/>
+        </SafeAreaView>
+    );
+};
+export default  HomeScreen;
---
+++ src/types/userData.tsx
@@ -0,0 +1,6 @@
+export interface UserData {
+    code:string,
+    name:string,
+    surname:string,
+
+}
